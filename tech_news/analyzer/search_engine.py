f555a8171d464cc125273dd70ca1f7e0ab2f484e
Robert Rodrigues <robert.rm131@gmail.com>
2022-11-16 17:03:44 -0300

    Feat: search by category

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -35,4 +35,8 @@ def search_by_tag(tag):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    user_search = {"$regex": rf"{category}", "$options": "i"}
+    query = {"category": user_search}
+    news_list = search_news(query)
+    response = [tuple((news["title"], news["url"])) for news in news_list]
+    return response
